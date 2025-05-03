74a65bbc753f4ad49aefad3440cd351b764a5e8a
Denis <deniskhuharev@gmail.com>
2025-05-03 18:10:01 +0700

    Create basic API and basic frontend template

--- users/movietheater/urls.py
+++ users/movietheater/urls.py
@@ -3,7 +3,5 @@ from . import views
 from .views import signup_view, login_view
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
+
 ]
--- users/users/settings.py
+++ users/users/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'movietheater',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,14 +51,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication', 
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'users.urls'
 
